0e31592b41eb49636d860a1d680d3b37eda98b3a
arcw76183450 <41956301+arcw76183450@users.noreply.github.com>
2023-11-30 22:07:50 +0530

    Final changes

---
+++ README.md
@@ -0,0 +1,2 @@
+# dice_roller
+An app using flutter which generates random dice rolls on button click
---
+++ lib/main.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+import 'package:dice_roller/gradient_container.dart';
+
+void main() {
+  runApp(
+    MaterialApp(
+      home: Scaffold(body: GradientContainer.orange()),
+    ),
+  );
+}
